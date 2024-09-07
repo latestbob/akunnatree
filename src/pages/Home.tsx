473a5a8bbecd9e95e554e675b647b969b0df02fa
latestbob <edidiongbobson@gmail.com>
2024-09-07 15:38:18 +0100

    complete login screen

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,7 +5,8 @@ import instagram from '../assets/instagram.png'
 import tiktok from '../assets/tiktok.png'
 import ux from '../assets/ux.png'
 import web from '../assets/web-link.png'
-
+import { motion, spring } from 'framer-motion';
+import whatsapp from '../assets/whatsapp.png';
 
 const Home = (): JSX.Element => {
 
@@ -15,7 +16,13 @@ const Home = (): JSX.Element => {
         {/* reference link for design here:https://linktreefreeclone.yoandev.co/ */}
             <section className="main p-0 m-0 w-full overflow-x-hidden min-h-screen bg-blue-100 py-10">
 
-                <div className="image-div py-5 text-center">
+                <motion.div 
+                    initial={{ opacity: 0, y:-100 }}
+                    animate={{ opacity: 1 , y:0}}
+                    transition={{delay:1, duration:0.8, type:'spring'}}
+            
+                
+                className="image-div py-5 text-center">
                     <img src={avater} className='w-1/4 md:w-1/6 rounded-full mx-auto'/>
 
                     <h1 className="intro text-2xl  md:text-4xl  mt-4 font-bold font-sans text-blue-900">Akunna Ndubuisi </h1>
@@ -24,26 +31,82 @@ const Home = (): JSX.Element => {
                     </h3>
 
                     <p className="text-center text-sm mt-5 md:text-lg px-4 md:px-20 font-medium text-blue-900">Helping You Elevate Your Beauty and Aesthetic Skills: <br />  I am a Cosmetic Scientist, Injector, and Aesthetic Educator</p>
-                </div>
+                </motion.div>
 
-                <div className="socials mt-4 mb-16 flex w-1/2 md:w-1/5 mx-auto max-h-16 justify-evenly align-middle">
+                <motion.div 
+                initial ={{
+                    x:-100,
+                    opacity:0
+                }}
+                animate={{
+                    opacity:1,
+                    x:0
+                }}
+                transition={{delay:1.2,duration:1.3, type:'tween'}}
+                
+                className="socials mt-4 mb-16 flex w-1/2 md:w-1/5 mx-auto max-h-16 justify-evenly align-middle">
 
 
-                        <img className='social w-10' src={instagram} />
-                        <img className='social w-10' src={tiktok} />
-                        <img className='social w-10' src={facebook} />
+                        <motion.img 
+                            whileHover={{
+                                scale:1.4
+                            }}
+                        className='social w-10' src={instagram} />
+                        <motion.img 
+                            whileHover={{
+                                scale:1.4
+                            }}
+                        className='social w-10' src={tiktok} />
+                        <motion.img
+                            whileHover={{
+                                scale:1.4
+                            }}
+                        className='social w-10' src={facebook} />
+
+                        <motion.img
+                            whileHover={{
+                                scale:1.4
+                            }}
+                        className='social w-10' src={whatsapp} />
 
 
-                </div>
+                </motion.div>
 
 
-                <div className="tree w-3/4  py-3 m-auto min-h-[40vh]" >
+                <div className="tree w-full md:w-3/4  py-3 m-auto px-3 md:px-0 grid grid-cols-1 md:grid-cols-2 gap-4" >
 
-                    <div className="bg-white w-1/2 py-5 rounded-lg min-h-14 px-8 border-2 border-amber-400">
+                    <div className="bg-white py-3 md:py-5 rounded-lg h-auto px-8  md:border-2 border-amber-400">
 
-                        <div className='flex justify-around w-1/2'>
+                        <div className='flex flex-row-reverse md:flex-row justify-between md:justify-around w-full md:w-1/2'>
                            <img src={ux} className="w-8 rounded" alt="" /> 
-                           <p className="font-medium ">Pluto Beauty Supplies</p>
+                           <p className="font-normal md:font-medium ">Pluto Beauty Supplies</p>
+                        </div>
+                    
+                    </div>
+
+                    <div className="bg-white py-3 md:py-5 rounded-lg h-auto px-8  md:border-2 border-amber-400">
+
+                        <div className='flex flex-row-reverse md:flex-row justify-between md:justify-around w-full md:w-1/2'>
+                           <img src={ux} className="w-8 rounded" alt="" /> 
+                           <p className="font-normal md:font-medium ">Pluto Beauty Supplies</p>
+                        </div>
+                    
+                    </div>
+
+                    <div className="bg-white py-3 md:py-5 rounded-lg h-auto px-8  md:border-2 border-amber-400">
+
+                        <div className='flex flex-row-reverse md:flex-row justify-between md:justify-around w-full md:w-1/2'>
+                           <img src={ux} className="w-8 rounded" alt="" /> 
+                           <p className="font-normal md:font-medium ">Pluto Beauty Cafe</p>
+                        </div>
+                    
+                    </div>
+
+                    <div className="bg-white py-3 md:py-5 rounded-lg h-auto px-8  md:border-2 border-amber-400">
+
+                        <div className='flex flex-row-reverse md:flex-row justify-between md:justify-around w-full md:w-1/2'>
+                           <img src={ux} className="w-8 rounded" alt="" /> 
+                           <p className="font-normal md:font-medium ">Pluto Beauty Supplies</p>
                         </div>
                     
                     </div>
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,11 +1,82 @@
 import React from 'react';
+import { useState } from 'react';
 
 
 const Login = ():JSX.Element => {
 
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const[showPassword, setShowPassword] = useState<boolean>(false);
+
+    
+
+    function handleEmailChange(e:React.ChangeEvent<HTMLInputElement>){
+
+        setEmail(e.target.value)
+    }
+
+    function handlePasswordChange(e:React.ChangeEvent<HTMLInputElement>){
+
+        setPassword(e.target.value)
+    }
+
     return (
         <>
-            <h2>This is the login page</h2>
+            <section className="flex flex-col justify-center px-5 md:px-0  m-0  overflow-x-hidden bg-blue-100 min-h-screen">
+
+                <div className='bg-white w-full md:w-2/5 rounded-lg min-h-[60vh] md:min-h-[50vh] m-auto py-16 md:py-8'>
+                
+                    <h2 className="text-center text-xl md:text-2xl font-medium font-sans">Login To Dashboard</h2>
+
+
+
+                    <form className="mt-3 px-5">
+
+
+                        <div className="form-group py-3">
+                            <label className="block text-sm font-medium text-gray-700">Email Address </label>
+                            <input
+                                onChange={handleEmailChange}
+                                type="email"
+                                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-200 focus:border-amber-200 sm:text-sm"
+                                placeholder="Enter your email address"
+                            />
+                        </div>
+
+                        <div className="form-group py-3">
+                            <label className="block text-sm font-medium text-gray-700">Secured Password</label>
+                            <input
+                                onChange={handlePasswordChange}
+                                type={showPassword ? 'text' : 'password'}
+                                className="mt-1 block w-full px-3 py-2 bg-white border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-amber-200 focus:border-amber-200 sm:text-sm"
+                                placeholder="Enter your password"
+                            />
+
+                            <div className="toggle my-3">
+                            <input type="checkbox" onChange={function(){
+                                setShowPassword(!showPassword)
+                            }} checked={showPassword} className='text-amber-500 ' /> <span className="px-4 text-sm">{showPassword ? 'Hide Password' : 'Show Password'}</span>
+                            </div>
+
+
+                            <br />
+
+
+                            <div className="buttondiv text-center">
+                                <button className="btn w-full rounded-md bg-amber-500 py-2 px-3 text-white font-sans font-medium text-base">Login To Dashboard</button>
+                            </div>
+                            
+                        </div>
+                                        
+                    </form>
+                
+                </div>
+
+
+                
+
+
+            </section>
         </>
     );
 }
